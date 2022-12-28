1ffcdd61264c42f2cbc0d6e96cd2e2153fe9442b
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-12-29 02:24:52 +0900

    APIのレスポンスからテキストを作ってLINEへ返す処理

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -20,10 +20,7 @@ class LineBotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text'] # 送られてきたテキストをそのまま代入
-          }
+          message = search_and_create_message(event.message['text'])
           client.reply_message(event['replyToken'], message)
         end
       end
@@ -52,5 +49,18 @@ class LineBotController < ApplicationController
     }
     response = http_client.get(url, query)
     response = JSON.parse(response.body)
+
+    text = ''                             # 空文字を初期値にすることでString型の変数と判断させる
+    response['hotels'].each do |hotel|
+      text <<
+      hotel[0]['hotelBasicInfo']['hotelName'] + "\n" +
+      hotel[0]['hotelBasicInfo']['hotelInformationUrl'] + "\n" + 
+      "\n"
+    end
+
+    message = {
+      type: 'text',
+      text: text
+    }
   end
 end

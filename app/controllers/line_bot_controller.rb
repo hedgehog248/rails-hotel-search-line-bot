8b3a35b0092f9981f87d390efb0b458a1484c939
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-12-28 12:38:05 +0900

    テキストをオウム返しする記述

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -2,12 +2,33 @@ class LineBotController < ApplicationController
   protect_from_forgery except: [:callback]
 
   def callback
+    # POSTリクエストからメッセージボディを文字列として取得する
     body = request.body.read
+
+    # 署名の検証
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       return head :bad_request
     end
+
+    # bodyのevents以下をハッシュに直して格納する
     events = client.parse_events_from(body)
+
+    # eventを取り出し、テキストかどうか評価して処理する
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text'] # 送られてきたテキストをそのまま代入
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+    head :ok
   end
 
   private
